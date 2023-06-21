64ec9301fe7c1db6ed6d02fad3d38fee64fbcb4f
Mikhail <mikhail@volkovlabs.io>
2023-06-20 20:42:52 -0400

    Formatting

--- src/constants/tests.ts
+++ src/constants/tests.ts
@@ -3,9 +3,9 @@
  */
 export const TestIds = {
   configEditor: {
-    root: 'data-testid config-editor',
-    fieldPath: 'data-testid config-editor field-path',
     fieldApiKey: 'data-testid config-editor field-api-key',
+    fieldPath: 'data-testid config-editor field-path',
+    root: 'data-testid config-editor',
   },
   queryEditor: {
     fieldQueryText: 'data-testid query-editor field-query-text',
